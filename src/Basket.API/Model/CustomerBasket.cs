f90ba4ffbd3af0a568f34c0d013d8feac2cac142
SchmitzChristopher <schmitz.christofer@live.de>
2025-06-02 14:53:30 +0000

    CustomerBasket XML-Dokumentiert

--- src/Basket.API/Model/CustomerBasket.cs
+++ src/Basket.API/Model/CustomerBasket.cs
@@ -1,13 +1,29 @@
 ﻿namespace eShop.Basket.API.Model;
 
+/// <summary>
+/// Repräsentiert einen Warenkorb eines Kunden.
+/// </summary>
 public class CustomerBasket
 {
+    /// <summary>
+    /// Die eindeutige Kennung des Käufers.
+    /// </summary>
     public string BuyerId { get; set; }
 
+    /// <summary>
+    /// Die Liste der Artikel im Warenkorb.
+    /// </summary>
     public List<BasketItem> Items { get; set; } = [];
 
+    /// <summary>
+    /// Erstellt eine neue Instanz von <see cref="CustomerBasket"/>.
+    /// </summary>
     public CustomerBasket() { }
 
+    /// <summary>
+    /// Erstellt eine neue Instanz von <see cref="CustomerBasket"/> mit einer angegebenen Käufer-ID.
+    /// </summary>
+    /// <param name="customerId">Die ID des Käufers.</param>
     public CustomerBasket(string customerId)
     {
         BuyerId = customerId;
